a6e496c3bd69488a4ca44e580881a69b68f97876
Abby Jacoby <abby.jacoby@gmail.com>
2023-02-05 15:15:41 -0700

    fix twitter api

--- Utilities/Twitter_rest.py
+++ Utilities/Twitter_rest.py
@@ -13,7 +13,7 @@ class TwitterRest:
     def get_twitter_media(self, screen_name, num):
         user_id = self.api.get_user(username=screen_name).data.id
         tweets = self.api.get_timelines(user_id, tweet_fields='created_at', expansions='attachments.media_keys',
-                                        media_fields='url')
+                                        media_fields='url', exclude=["retweets", "replies"], max_results=100)
 
         test_list = {text.url for text in tweets.includes.media}
         return_list = list(filter(lambda item: item is not None, test_list))
