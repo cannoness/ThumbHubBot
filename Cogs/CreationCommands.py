c96efe3318f6b47709f1510886000254cc4e48b4
Abby Jacoby <abby.jacoby@gmail.com>
2023-02-05 10:54:38 -0700

    user who did it

--- Cogs/CreationCommands.py
+++ Cogs/CreationCommands.py
@@ -426,7 +426,7 @@ class CreationCommands(commands.Cog):
             message = f"""Viewing {", ".join([f"[{image['title']}]({image['url']})" for image in 
                                               results[:self._check_your_privilege(ctx)]])}.\n
                         A Selection from today's [Daily Deviations](https://www.deviantart.com/daily-deviations)"""
-            await self._send_art_results(ctx, channel, results, message)
+            await self._send_art_results(ctx, channel, results, message, usernames=[ctx.message.author.display_name])
             self.da_rest.add_coins(ctx.message.author.id, None)
         except Exception as ex:
             print(ex, flush=True)
