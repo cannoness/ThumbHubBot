c8daee7cda94ef4966626fab561d4628e16f934e
Abby Jacoby <abby.jacoby@gmail.com>
2023-05-21 11:24:02 -0600

    still no message in the channel?

--- Cogs/Events.py
+++ Cogs/Events.py
@@ -11,17 +11,14 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        channel = await self.bot.fetch_channel(ctx.channel)
         if isinstance(error, commands.MissingRequiredArgument):
             ctx.command.reset_cooldown(ctx)
-            await channel.send(f"{error}")
-            print("command didn't work.")
+            return await ctx.send(f"{error}")
         if isinstance(error, commands.errors.CommandOnCooldown):
             minutes, seconds = divmod(error.retry_after, 60)
-            await channel.send(f"This command is on cooldown for user {ctx.message.author.display_name}, "
-                               f"try again after {int(minutes)}m {int(seconds)}s.", ephemeral=True)
-            print("command didn't work.")
-        await channel.send(f"{error}")
+            return await ctx.send(f"This command is on cooldown for user {ctx.message.author.display_name}, "
+                                  f"try again after {int(minutes)}m {int(seconds)}s.", ephemeral=True)
+        return await ctx.send(f"{error}")
 
 
 async def setup(bot):
